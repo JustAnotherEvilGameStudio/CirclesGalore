62674a4049b5725d703b5ca959b78d8272c4c384
Nejc <ethoarx@gmail.com>
2019-11-06 21:10:27 +0100

    test(Bodies): Try a RigidBody

--- CirclesGalore.Game/CirclesGalore.cs
+++ CirclesGalore.Game/CirclesGalore.cs
@@ -4,24 +4,46 @@ using osuTK;
 using osuTK.Graphics;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Allocation;
+using osu.Framework.Physics;
 using OGame = osu.Framework.Game;
 
 namespace CirclesGalore.Game
 {
     public class CirclesGaloreGame : OGame
     {
+        private RigidBodySimulation sim;
         private Box box;
 
         [BackgroundDependencyLoader]
         private void load()
         {
+            Child = sim = new RigidBodySimulation { RelativeSizeAxes = Axes.Both };
+
+            RigidBodyContainer<Drawable> rbc = new RigidBodyContainer<Drawable>
+            {
+                Child = new Box
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    Size = new Vector2(150, 150),
+                    Colour = Color4.Tomato,
+                },
+                Position = new Vector2(500, 500),
+                Size = new Vector2(200, 200),
+                Rotation = 45,
+                Colour = Color4.Tomato,
+                Masking = true,
+            };
+
             Add(box = new Box
             {
                 Anchor = Anchor.Centre,
                 Origin = Anchor.Centre,
                 Size = new Vector2(150, 150),
-                Colour = Color4.Tomato
+                Colour = Color4.Aqua
             });
+
+            sim.Add(rbc);
         }
 
         protected override void Update()
--- CirclesGalore.Main/Program.cs
+++ CirclesGalore.Main/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using CirclesGalore.Game;
 using CirclesGalore.Game.Tests;
 using osu.Framework.Platform;
@@ -9,17 +10,29 @@ namespace CirclesGalore.Runner.Desktop
 {
     class Program
     {
-        [MTAThread]
-        public static void Main()
+        [STAThread]
+        public static void Main(string[] args)
         {
-            var testMode = true;
+            var testMode = false;
 
-            using (GameHost host = Host.GetSuitableHost(@"CirclesGalore"))
-            using (OGame game = new CirclesGaloreGame())
-            using (OGame gameTests = new CirclesGaloreGameTests())
+            switch (args.FirstOrDefault() ?? string.Empty)
             {
-                if (testMode) host.Run(gameTests);
-                else host.Run(game);
+                case "--test":
+                    testMode = true;
+                    break;
+                default:
+                    testMode = false;
+                    break;
+            }
+
+            using (DesktopGameHost host = Host.GetSuitableHost(@"CirclesGalore"))
+            {
+                using (OGame game = new CirclesGaloreGame())
+                using (OGame gameTests = new CirclesGaloreGameTests())
+                {
+                    if (testMode) host.Run(gameTests);
+                    else host.Run(game);
+                }
             }
         }
     }
